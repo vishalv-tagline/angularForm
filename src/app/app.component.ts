eb3dfd983d6c2fc30e38089828efc3f1cade4335
vishalv-tagline <vishalv.tagline@gmail.com>
2023-01-03 18:57:14 +0530

    Implementing insert,Delete in project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Studs } from './common';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularForm';
+
+  @ViewChild('myForm') myForm!: NgForm;
+
+  studs: Studs[] = [
+    {
+      fname: 'Variya',
+      lname: 'Vishal',
+      gender: 'Male',
+      mobile: '9924769731',
+      email: 'vishal11@gmail.com'
+    },
+    {
+      fname: 'Maurya',
+      lname: 'Arvindbhai',
+      gender: 'Male',
+      mobile: '985652251',
+      email: 'arvind11@gmail.com'
+    },
+  ]
+
+  addStud() {
+    // console.log('myForms :>> ', this.myForm.value);
+    this.studs.push(this.myForm.value)
+    this.myForm.reset();
+  }
+
+  onUpdate(i: number) {
+
+  }
+
+  onDelete(i: number) {
+    this.studs.splice(i, 1)
+  }
 }
---
+++ src/app/common.ts
@@ -0,0 +1,7 @@
+export interface Studs {
+    fname: string,
+    lname: string,
+    gender: string,
+    mobile: string,
+    email: string
+}
